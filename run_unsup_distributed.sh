6226770f2099722be1e97a0899a8a96f0ca25bd5
Jeremiah <1442943357@qq.com>
2022-04-18 12:39:42 +0000

    update sources for main debug

---
+++ run_unsup_distributed.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# In this example, we show how to train SimCSE using multiple GPU cards and PyTorch's distributed data parallel on supervised NLI dataset.
+# Set how many GPUs to use
+NUM_GPU=8
+# export CUDA_VISIBLE_DEVICES="0,1,2,3"
+# Randomly set a port number
+# If you encounter "address already used" error, just run again or manually set an available port id.
+PORT_ID=$(expr $RANDOM + 1000)
+
+# Allow multiple threads
+export OMP_NUM_THREADS=8
+
+# Use distributed data parallel
+# If you only want to use one card, uncomment the following line and comment the line with "torch.distributed.launch"
+# python train.py \
+python -m torch.distributed.launch \
+    --nproc_per_node $NUM_GPU \
+    --master_port $PORT_ID train.py \
+    --model_name_or_path bert-base-uncased \
+    --train_file data/wiki1m_for_simcse.txt \
+    --output_dir result/princeton-unsup-bert \
+    --num_train_epochs 1 \
+    --per_device_train_batch_size 64 \
+    --learning_rate 3e-5 \
+    --max_seq_length 32 \
+    --evaluation_strategy steps \
+    --metric_for_best_model stsb_spearman \
+    --load_best_model_at_end \
+    --eval_steps 125 \
+    --pooler_type cls \
+    --mlp_only_train \
+    --overwrite_output_dir \
+    --temp 0.05 \
+    --do_train \
+    --do_eval \
+    --fp16 \
+    "$@"
