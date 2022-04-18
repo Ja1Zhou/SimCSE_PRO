6da800bf46a2ead88b270b674f0ba4df33082a12
Jay Zhou <1442943357@qq.com>
2022-04-18 13:08:40 +0000

    some results

--- debug_eval.sh
+++ debug_eval.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export CUDA_VISIBLE_DEVICES=3
-python evaluation.py \
+python -m debugpy --listen 127.0.0.1:5678 --wait-for-client evaluation.py \
     --model_name_or_path princeton-nlp/unsup-simcse-bert-base-uncased\
-    --pooler cls \
+    --pooler cls_before_pooler \
     --task_set sts \
     --mode test
