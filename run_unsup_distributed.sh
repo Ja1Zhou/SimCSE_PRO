500049d6386ebebecc549e768cbac1b59fcf90a9
Jeremiah <1442943357@qq.com>
2022-04-19 13:50:10 +0000

    for reproducing avg results

--- run_unsup_distributed.sh
+++ run_unsup_distributed.sh
@@ -2,8 +2,8 @@
 
 # In this example, we show how to train SimCSE using multiple GPU cards and PyTorch's distributed data parallel on supervised NLI dataset.
 # Set how many GPUs to use
-NUM_GPU=8
-# export CUDA_VISIBLE_DEVICES="0,1,2,3"
+NUM_GPU=4
+export CUDA_VISIBLE_DEVICES="4,5,6,7"
 # Randomly set a port number
 # If you encounter "address already used" error, just run again or manually set an available port id.
 PORT_ID=$(expr $RANDOM + 1000)
@@ -19,16 +19,16 @@ python -m torch.distributed.launch \
     --master_port $PORT_ID train.py \
     --model_name_or_path bert-base-uncased \
     --train_file data/wiki1m_for_simcse.txt \
-    --output_dir result/reproduce-unsup-bert-cls-before-pooler \
+    --output_dir result/reproduce-unsup-bert-avg \
     --num_train_epochs 1 \
-    --per_device_train_batch_size 64 \
+    --per_device_train_batch_size 16 \
     --learning_rate 3e-5 \
     --max_seq_length 32 \
     --evaluation_strategy steps \
     --metric_for_best_model stsb_spearman \
     --load_best_model_at_end \
     --eval_steps 125 \
-    --pooler_type cls \
+    --pooler_type avg \
     --mlp_only_train \
     --overwrite_output_dir \
     --temp 0.05 \
--- run_unsup_example.sh
+++ run_unsup_example.sh
@@ -3,11 +3,11 @@
 # In this example, we show how to train SimCSE on unsupervised Wikipedia data.
 # If you want to train it with multiple GPU cards, see "run_sup_example.sh"
 # about how to use PyTorch's distributed data parallel.
-
+export CUDA_VISIBLE_DEVICES=7
 python train.py \
     --model_name_or_path bert-base-uncased \
     --train_file data/wiki1m_for_simcse.txt \
-    --output_dir result/my-unsup-simcse-bert-base-uncased \
+    --output_dir result/reproduce-unsup-bert-avg \
     --num_train_epochs 1 \
     --per_device_train_batch_size 64 \
     --learning_rate 3e-5 \
@@ -16,11 +16,10 @@ python train.py \
     --metric_for_best_model stsb_spearman \
     --load_best_model_at_end \
     --eval_steps 125 \
-    --pooler_type cls \
+    --pooler_type avg \
     --mlp_only_train \
     --overwrite_output_dir \
     --temp 0.05 \
     --do_train \
-    --do_eval \
     --fp16 \
     "$@"
