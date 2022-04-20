3d6aaded83cbf6a194c939b92f75f7b67a839193
Jeremiah <1442943357@qq.com>
2022-04-20 05:00:52 +0000

    gg for baseline avg

--- convert.sh
+++ convert.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 python simcse_to_huggingface.py \
-    --path /home/zhejian/SimCSE/result/reproduce-unsup-bert-cls-before-pooler
+    --path result/reproduce-unsup-bert-avg
--- eval.sh
+++ eval.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export CUDA_VISIBLE_DEVICES=3
 python evaluation.py \
-    --model_name_or_path /home/zhejian/SimCSE/result/reproduce-unsup-bert-cls-before-pooler\
-    --pooler cls_before_pooler \
+    --model_name_or_path result/reproduce-unsup-bert-avg\
+    --pooler avg \
     --task_set sts \
     --mode test
