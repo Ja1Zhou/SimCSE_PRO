de35aa4c1b843f55986a2ca6fb109c123122f6a9
Jeremiah <1442943357@qq.com>
2022-04-20 08:53:52 +0000

    gg for better baseline results

--- eval.sh
+++ eval.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export CUDA_VISIBLE_DEVICES=3
 python evaluation.py \
-    --model_name_or_path result/reproduce-unsup-bert-avg\
-    --pooler avg \
+    --model_name_or_path result/reproduce-unsup-bert-cls\
+    --pooler cls_before_pooler \
     --task_set sts \
     --mode test
