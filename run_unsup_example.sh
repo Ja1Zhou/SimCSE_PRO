ff1273a575a2508e4f526465d9d064fa04368605
Jeremiah <1442943357@qq.com>
2022-04-20 05:30:50 +0000

    tmp reproduce

--- run_unsup_example.sh
+++ run_unsup_example.sh
@@ -3,11 +3,11 @@
 # In this example, we show how to train SimCSE on unsupervised Wikipedia data.
 # If you want to train it with multiple GPU cards, see "run_sup_example.sh"
 # about how to use PyTorch's distributed data parallel.
-export CUDA_VISIBLE_DEVICES=7
+export CUDA_VISIBLE_DEVICES=0
 python train.py \
     --model_name_or_path bert-base-uncased \
     --train_file data/wiki1m_for_simcse.txt \
-    --output_dir result/reproduce-unsup-bert-avg \
+    --output_dir result/reproduce-unsup-bert-cls \
     --num_train_epochs 1 \
     --per_device_train_batch_size 64 \
     --learning_rate 3e-5 \
@@ -16,7 +16,7 @@ python train.py \
     --metric_for_best_model stsb_spearman \
     --load_best_model_at_end \
     --eval_steps 125 \
-    --pooler_type avg \
+    --pooler_type cls \
     --mlp_only_train \
     --overwrite_output_dir \
     --temp 0.05 \
