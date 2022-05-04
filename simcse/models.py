3a5ddf3a5bb0bbc7b40002b0d121283020077bf9
Jeremiah <1442943357@qq.com>
2022-05-04 12:45:03 +0000

    tmp clean

--- simcse/models.py
+++ simcse/models.py
@@ -1,19 +1,9 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 import torch.distributed as dist
 
-import transformers
-from transformers import RobertaTokenizer
 from transformers.models.roberta.modeling_roberta import RobertaPreTrainedModel, RobertaModel, RobertaLMHead
 from transformers.models.bert.modeling_bert import BertPreTrainedModel, BertModel, BertLMPredictionHead
-from transformers.activations import gelu
-from transformers.file_utils import (
-    add_code_sample_docstrings,
-    add_start_docstrings,
-    add_start_docstrings_to_model_forward,
-    replace_return_docstrings,
-)
 from transformers.modeling_outputs import SequenceClassifierOutput, BaseModelOutputWithPoolingAndCrossAttentions
 
 class MLPLayer(nn.Module):
