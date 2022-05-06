6c8bed6de5273c990dd95d5a1d8c4e9ceb3f37c4
Jeremiah <1442943357@qq.com>
2022-05-06 14:23:16 +0000

    update cleaned data

--- eval.sh
+++ eval.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export CUDA_VISIBLE_DEVICES=7
 python evaluation.py \
-    --model_name_or_path result/new-unsup-bert-avg \
+    --model_name_or_path result/wiki-unique-unsup-bert-avg \
     --pooler_type avg \
     --task_set sts \
     --mode test
