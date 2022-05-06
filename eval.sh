0a4663978993c9e3fadcaec214d0ff71d795499b
Jeremiah <1442943357@qq.com>
2022-05-06 14:42:55 +0000

    simple change

--- eval.sh
+++ eval.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export CUDA_VISIBLE_DEVICES=7
 python evaluation.py \
-    --model_name_or_path result/wiki-unique-unsup-bert-avg \
+    --model_name_or_path $1 \
     --pooler_type avg \
     --task_set sts \
     --mode test
