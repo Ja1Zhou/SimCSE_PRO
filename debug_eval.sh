50e4fd7ae229fe3888d407da995c213562054dc6
Jeremiah <1442943357@qq.com>
2022-05-21 17:30:48 +0800

    update better yet unsatisfactory result

--- debug_eval.sh
+++ debug_eval.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 export CUDA_VISIBLE_DEVICES=7
-python -m debugpy --listen 127.0.0.1:5678 --wait-for-client evaluation.py
+python -m debugpy --listen 127.0.0.1:5678 evaluation.py \
+    +data_args=prompt \
+    +model_args=self_weighted_prompt_bert_unsup \
+    +trainer_args=self_weighted_prompt_bert \
--- self_weighted_prompt_bert_unsup.sh
+++ self_weighted_prompt_bert_unsup.sh
@@ -3,4 +3,5 @@ export CUDA_VISIBLE_DEVICES=7
 python train.py \
     +data_args=prompt \
     +model_args=self_weighted_prompt_bert_unsup \
+    ++model_args.freeze_backbone=False \
     +trainer_args=self_weighted_prompt_bert \
