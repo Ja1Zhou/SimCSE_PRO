7c7e011835d37f39786744cdbd3b1789997d0f47
Jeremiah <1442943357@qq.com>
2022-05-21 17:37:30 +0800

    simple update

--- eval.sh
+++ eval.sh
@@ -3,4 +3,5 @@ export CUDA_VISIBLE_DEVICES=7
 python evaluation.py \
     +data_args=prompt \
     +model_args=self_weighted_prompt_bert_unsup \
+    ++model_args.inference_prompt=2 \
     +trainer_args=self_weighted_prompt_bert \
