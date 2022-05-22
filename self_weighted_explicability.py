eb10e57c3e9cb5ca71fc22d02a4090d7a7f42198
Jeremiah <1442943357@qq.com>
2022-05-22 18:48:10 +0800

    update explicability results

--- self_weighted_explicability.py
+++ self_weighted_explicability.py
@@ -37,7 +37,7 @@ def main(cfg):
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     model = model.to(device)
     model.eval()
-    input_sentences = explicability_args.input_sentences
+    input_sentences = list(explicability_args.input_sentences)
     encoded_input_sentences = tokenizer(input_sentences, return_tensors="pt", padding=True, truncation=True, max_length=512).to(device)
     output_hidden_states = True
     return_dict = True
@@ -46,14 +46,24 @@ def main(cfg):
     for k in model_args.model_forward_args:
         model_forward_args[k] = eval(k)
     encoded_input_sentences.update(model_forward_args)
+    def get_unique_words(list_of_tokens):
+        for i in range(len(list_of_tokens)):
+            if list_of_tokens[i] in list_of_tokens[:i]:
+                list_of_tokens[i] = list_of_tokens[i]+"_"
+        return list_of_tokens
     with torch.no_grad():
         outputs = model(**encoded_input_sentences)
-        for i in len(encoded_input_sentences):
-            plt.bar(tokenizer.convert_ids_to_tokens(encoded_input_sentences["input_ids"][i]),outputs.hidden_states[0].detach().numpy())
-            plt.ylabel("MLP output weights")
+        for i in range(len(encoded_input_sentences["input_ids"])):
+            plt.figure()
+            plt.bar(get_unique_words(tokenizer.convert_ids_to_tokens(encoded_input_sentences["input_ids"][i])),outputs.hidden_states[i].cpu().detach().numpy())
+            plt.bar(get_unique_words(tokenizer.convert_ids_to_tokens(encoded_input_sentences["input_ids"][i])),(outputs.hidden_states[i].where(outputs.hidden_states[i]>1/torch.sum(encoded_input_sentences["attention_mask"][i]),torch.zeros_like(outputs.hidden_states[i]))).cpu().detach().numpy())
+            plt.ylabel("Scorer Output Weights")
+            plt.xlabel("Sentence Tokens")
+            plt.title("Visualizing Self-Weighted Outputs")
             plt.xticks(rotation=90)
+            plt.tight_layout()
             plt.show()
-            plt.savefig(f"{training_args.output_dir}/mlp_weights_{i}.png")
+            plt.savefig(f"{explicability_args.output_dir}/mlp_weights_{i}.png")
 
 if __name__ == "__main__":
     main()
